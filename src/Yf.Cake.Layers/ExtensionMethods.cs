ae4a47393aa308effae624145411ad0c77441de7
Yavor Fingarov <yavor.fingarov@outlook.com>
2024-12-27 23:22:19 +0200

    Fixed loc

--- src/Yf.Cake.Layers/ExtensionMethods.cs
+++ src/Yf.Cake.Layers/ExtensionMethods.cs
@@ -14,7 +14,7 @@
             return value switch
             {
                 < 1_000 => value.ToString(CultureInfo.InvariantCulture),
-                < 10_000 => $"{(double)value / 1_000:0.0}k",
+                < 10_000 => $"{(double)value / 1_000:0.#}k",
                 < 1_000_000 => $"{(double)value / 1_000:0}k",
                 _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Got an unexpected value.")
             };
